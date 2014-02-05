c8ac4ba0802ea32792eb837d79ba7ad297805131
Matt Clifton <mattc@geocaching.com>
2014-02-05 11:37:44 -0800

    fixed template_context to copy templates corectly during rake task

--- spec/template_context.rb
+++ spec/template_context.rb
@@ -1,10 +1,23 @@
 shared_context 'templates' do
   before(:all) do
-    files = Dir['../mssqlserver_alwayson/templates/default/*']
-    FileUtils.cp_r(files, '../mssqlserver_alwayson_tests/templates/default')
+    make_template_directory
+    files = Dir["#{source}/*"]
+    FileUtils.cp_r(files, destination)
   end
 
   after(:all) do
-    FileUtils.remove(Dir['../mssqlserver_alwayson_tests/templates/default/*'])
+    FileUtils.remove(Dir["#{destination}/*"])
+  end
+
+  def make_template_directory
+    FileUtils.mkdir_p(destination)
+  end
+
+  def source
+    @source ||= "#{File.expand_path('../../../mssqlserver_alwayson/templates/default/', __FILE__)}"
+  end
+
+  def destination
+    @destination ||= File.expand_path('../../templates/default/', __FILE__)
   end
 end
